d20373569a76d815b1d75058a2d7b9e38b7a7bb9
John Yeary <jyeary@bluelotussoftware.com>
2014-03-28 16:23:30 -0400

    Initial import

---
+++ src/main/java/com/bluelotusoftware/example/jsf/IndexBean.java
@@ -0,0 +1,18 @@
+package com.bluelotusoftware.example.jsf;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author John Yeary <jyeary@bluelotussoftware.com>
+ * @version 1.0
+ */
+@ManagedBean
+@ViewScoped
+public class IndexBean implements Serializable {
+
+    private static final long serialVersionUID = -730798292453234258L;
+
+}
