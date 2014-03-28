60af4f8ca0d22feecebb55a89bcef55c3b4f4e2e
John Yeary <jyeary@bluelotussoftware.com>
2014-03-28 16:48:47 -0400

    Added examples

--- src/main/java/com/bluelotusoftware/example/jsf/IndexBean.java
+++ src/main/java/com/bluelotusoftware/example/jsf/IndexBean.java
@@ -3,6 +3,8 @@ package com.bluelotusoftware.example.jsf;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  *
@@ -14,5 +16,28 @@ import javax.faces.bean.ViewScoped;
 public class IndexBean implements Serializable {
 
     private static final long serialVersionUID = -730798292453234258L;
+    @NotNull(message = "The value can not be null")
+    private String value;
+    @NotEmpty
+    private String value2;
+
+    public IndexBean() {
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue2() {
+        return value2;
+    }
+
+    public void setValue2(String value2) {
+        this.value2 = value2;
+    }
 
 }
