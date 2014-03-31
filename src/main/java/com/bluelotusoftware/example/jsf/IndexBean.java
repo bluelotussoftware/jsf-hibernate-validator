535682925eed3030e9a5e5726eae728df4707293
John Yeary <jyeary@bluelotussoftware.com>
2014-03-31 10:30:30 -0400

    Added locale and additional examples.

--- src/main/java/com/bluelotusoftware/example/jsf/IndexBean.java
+++ src/main/java/com/bluelotusoftware/example/jsf/IndexBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
@@ -16,10 +17,16 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class IndexBean implements Serializable {
 
     private static final long serialVersionUID = -730798292453234258L;
-    @NotNull(message = "The value can not be null")
+    private String locale = "en";
+
+    @NotNull
     private String value;
     @NotEmpty
     private String value2;
+    @NotBlank
+    private String value3;
+    @NotBlank(message = "{BLANK}")
+    private String value4;
 
     public IndexBean() {
     }
@@ -38,6 +45,30 @@ public class IndexBean implements Serializable {
 
     public void setValue2(String value2) {
         this.value2 = value2;
+    }
+
+    public String getValue3() {
+        return value3;
+    }
+
+    public void setValue3(String value3) {
+        this.value3 = value3;
+    }
+
+    public String getValue4() {
+        return value4;
+    }
+
+    public void setValue4(String value4) {
+        this.value4 = value4;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
     }
 
 }
