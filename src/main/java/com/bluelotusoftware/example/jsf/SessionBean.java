ed6fa87bd07c9b3a426cafbdfdc41f2dd9828997
John Yeary <jyeary@bluelotussoftware.com>
2016-03-23 17:53:19 -0400

    1. Modified code for AJAX. 2. Added additional supported locales. 3. Updated to HTML5.

--- src/main/java/com/bluelotusoftware/example/jsf/SessionBean.java
+++ src/main/java/com/bluelotusoftware/example/jsf/SessionBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 John Yeary <jyeary@bluelotussoftware.com>.
+ * Copyright 2016 Blue Lotus Software, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
